b17d608b9b93b72442e6b3776e4cb2a89084c1ed
Fabian Hanso <fabian.hanso@accenture.com>
2026-01-02 18:18:58 +0100

    update footer to 2026

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -91,7 +91,7 @@ export default function Footer() {
           ))}
         </div>
         <p className="mt-10 text-center text-sm/6 text-gray-400">
-          &copy; 2025 Unit Zero Airsoft - Revice Media
+          &copy; 2026 Unit Zero Airsoft - Revice Media
         </p>
       </div>
     </footer>
